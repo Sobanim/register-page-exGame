fd587e46c2cc87e1d9526d8ba2e734df4a3a828a
Dmytro S <sobolev.dmirtiy@gmail.com>
2024-12-27 20:01:55 +0100

    Fix eslint

--- src/app/components/multiLangBtn/multiLangBtn.tsx
+++ src/app/components/multiLangBtn/multiLangBtn.tsx
@@ -3,11 +3,8 @@ import React, {useState} from 'react';
 import styles from './multiLangBtn.module.scss'
 import Image from "next/image";
 
-type Language = 'EN';
-
 const MultiLangBtn = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [selectedLanguage, setSelectedLanguage] = useState<Language>('EN');
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -19,7 +16,7 @@ const MultiLangBtn = () => {
     return (
         <div className={styles['multiLang-btn']}>
             <button onClick={toggleMenu} className={styles['selected-lang']}>
-                <Image src={langFlags[selectedLanguage]} width={20} height={15} alt='EN' />
+                <Image src={langFlags['EN']} width={20} height={15} alt='EN' />
             </button>
         </div>
     );
